486af362f20de480ccc9e71472e2c72dcac38111
Jason Gilbert <0jason.gilbert@gmail.com>
2019-03-22 17:22:19 -0300

    Pathfinding faster.. or broken..

--- processors/movement.py
+++ processors/movement.py
@@ -1,6 +1,7 @@
 import esper
 
 from components.actor.actor import ActorComponent
+from components.player import PlayerComponent
 from components.position import PositionComponent
 from components.tile import TileComponent
 from components.velocity import VelocityComponent
@@ -12,12 +13,17 @@ class MovementProcessor(esper.Processor):
     def process(self):
         for ent, (actor, vel, pos) in self.world.get_components(ActorComponent, VelocityComponent, PositionComponent):
             
-            for ent_blocker, (pos_blocker) in self.world.get_component(PositionComponent):
+            for ent_blocker, (actor, pos_blocker) in self.world.get_components(ActorComponent, PositionComponent):
                 if pos_blocker.x == pos.x + vel.dx and pos_blocker.y == pos.y + vel.dy:
-                    if self.world.has_component(ent_blocker, ActorComponent) or (self.world.has_component(ent_blocker, TileComponent) and self.world.component_for_entity(ent_blocker, TileComponent).blocks_path):
-                        break
-            else:
-                pos.x += vel.dx
-                pos.y += vel.dy
+                    return
+        
+            if self.world.has_component(ent, PlayerComponent):
+                # Player may run into walls, whereas AI uses the dijkstra map to navigate.
+                for ent_tile, (pos_tile, tile) in self.world.get_components(PositionComponent, TileComponent):
+                    if pos_tile.x == pos.x + vel.dx and pos_tile.y == pos.y + vel.dy and tile.blocks_path:
+                        return
+
+            pos.x += vel.dx
+            pos.y += vel.dy
 
             self.world.remove_component(ent, VelocityComponent)
