a549725e924a26a27643f08694d72712b5e2f3be
Shoes01 <0jason.gilbert@gmail.com>
2019-06-04 21:36:32 -0300

    Display equipped items in the character sheet

--- components/actor/equipment.py
+++ components/actor/equipment.py
@@ -1,6 +1,3 @@
 class EquipmentComponent():
     def __init__(self, equipment=None):
-        if equipment:
-            self.equipment = equipment # Store only the IDs of the item entities.
-        else:
-            self.equipment = []
+        self.equipment = [] if equipment is None else equipment
--- processors/sub/character_sheet.py
+++ processors/sub/character_sheet.py
@@ -1,6 +1,9 @@
 from _data import UI_COLORS
 from _helper_functions import generate_stats
-
+from components.actor.equipment import EquipmentComponent
+from components.item.skill import ItemSkillComponent
+from components.item.slot import SlotComponent
+from components.name import NameComponent
 from components.soul import SoulComponent
 from components.stats import StatsComponent
 
@@ -10,8 +13,17 @@ def render_character_sheet(world):
     
     console, _, _, w, h = world.consoles['map']
     color = UI_COLORS['text']
+    equipped_items = generate_equipped_items(world)
     player_base_stats = world.component_for_entity(1, StatsComponent).__dict__
     player_stats = generate_stats(1, world)
+    slots = {
+        'mainhand': 'Mainhand:',
+        'offhand': 'Offhand:',
+        'torso': 'Torso:',
+        'head': 'Head:',
+        'feet': 'Feet:',
+        'accessory': 'Accessory:'
+        }
     soul_stats = world.component_for_entity(1, SoulComponent).soul
     titles = {
         'attack': 'Attack:',
@@ -50,3 +62,34 @@ def render_character_sheet(world):
         i += 1
 
     # Displays item slots and their items
+    ### Mainhand: Sword
+    ###   Skill: Lunge
+    ###   Description: This is where the skill description goes.
+    x, y = 40, 5
+    console.print(x - 1, y - 1, 'Equipped Items:', color)
+    i = 0
+    for key in slots:
+        item_name, item_skill = None, None
+        if equipped_items.get(key):
+            item_name, item_skill = equipped_items[key]
+
+        text_1 = f"{slots[key]} {item_name}"
+        text_2 = f"  Skill: {str(item_skill).capitalize()}"
+        
+        console.print(x, y + i, text_1, color)
+        i += 1
+        console.print(x, y + i, text_2, color)
+        i += 2
+
+def generate_equipped_items(world):
+    equipment = world.component_for_entity(1, EquipmentComponent).equipment
+    equipped_items = {}
+
+    for item in equipment:
+        slot = world.component_for_entity(item, SlotComponent).slot
+        name = world.component_for_entity(item, NameComponent)._name
+        skill = "None" if not world.has_component(item, ItemSkillComponent) else world.component_for_entity(item, ItemSkillComponent).name
+
+        equipped_items[slot] = (name, skill)
+    
+    return equipped_items
