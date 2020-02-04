03a2ffeb1b38642540cb74bbe0cb09d656f11538
Shoes01 <0jason.gilbert@gmail.com>
2020-02-04 16:48:55 -0400

    First pass at making menu.py use attrs

--- menu.py
+++ menu.py
@@ -1,55 +1,53 @@
+import attr
+import esper
+
 from _data import map
+from typing import Any, Dict, List
 
-class PopupChoiceCondition():
+@attr.s(slots=True, auto_attribs=True)
+class PopupChoiceCondition:
     ' This is a single condition to a popup menu choice. '
-    def __init__(self, description, valid=True):
-        self.description = description
-        self.valid = valid
+    description: str
+    valid: bool
 
+@attr.s(slots=True, auto_attribs=True, kw_only=True)
 class PopupChoiceResult():
     ' This is the result of making a menu choice. '
-    def __init__(self, result, processor):
-        self.result = result
-        self.processor = processor
+    result: Dict[str, int] = attr.Factory(dict) # entity name, entity ID.
+    processor: esper.Processor # Could also put Any here if this doesn't work.
 
+@attr.s(slots=True, auto_attribs=True, kw_only=True)
 class PopupChoice():
     ' This is a single entry into the popup menu. '
-    def __init__(self, name, key, results, valid=True, description=None, conditions=None):
-        self.conditions = [] if conditions is None else conditions # type: list of PopupChoiceConditions
-        self.description = "" if description is None else description
-        self.name = name     # The name of the choice.
-        self.key = key       # The key to select this choice.
-        self.results = results # type: list of PopupMenuResults
-        self._valid = valid   # If this is False, then the option is greyed out (at the moment, it can still be selected).
+    conditions: List[PopupChoiceCondition] = attr.Factory(list)
+    description: str = ""
+    key: str # The key to select this choice.
+    name: str
+    results: List[PopupChoiceResult] = attr.Factory(list)
 
     @property
     def valid(self):
-        if len(self.conditions) == 0:
-            return self._valid
+        for condition in self.conditions:
+            if condition.valid is False:
+                return False
         else:
-            for condition in self.conditions:
-                if condition.valid is False:
-                    return False
-            else:
-                return True
-        
+            return True
+
+@attr.s(slots=True, auto_attribs=True, kw_only=True)
 class PopupMenu():
     ' This contains the input and render information for a popup menu. '
-    def __init__(self, title, contents=None, include_esc=True, x=10, y=5, w=map.w-20, h=map.h-10, auto_close=True, include_description=None, reveal_all=True):
-        self.title = title
-        self.contents = [] # type: list of PopupChoices
-        
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        
-        self.auto_close = auto_close # If True, all menus will be closed upon selecting a choice.
-        self.include_description = include_description # If not None, it will include all the ingredients necessary to form a description.
-        self.include_esc = include_esc # If True, a choice to close the menu will be printed at the bottom of the menu.
-        self.reveal_all = reveal_all # If this is False, then invalid options are shown as ???s.
-        
-        ### Additional information that could be added later:
-        # Subtitle
-        # Category bool: if set to true, the renderer will sort items by their type. (items don't have a type yet)
-        # Equipped bool: if set to true, the equipped items will be listed on the side?
+    title: str
+    contents: List[PopupChoice] = attr.Factory(list)
+    x: int = 10
+    y: int = 5
+    w: int = map.w - 20
+    h: int = map.h - 10
+    auto_close: bool = True # If True, all menus will be closed upon selecting a choice.
+    include_description: Any = None # What is this? "Is it dict = Dict[str, int] # entity name, entity ID?
+    include_esc: bool = True # If True, a choice to close the menu will be printed at the bottom of the menu.
+    reveal_all: bool = True # If this is False, then invalid options are shown as ???s.
+    
+    ### Additional information that could be added later:
+    # Subtitle
+    # Category bool: if set to true, the renderer will sort items by their type. (items don't have a type yet)
+    # Equipped bool: if set to true, the equipped items will be listed on the side?
