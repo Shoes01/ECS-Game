b7167eea54298f4671511211eda73971de736d67
Shoes01 <0jason.gilbert@gmail.com>
2020-02-16 22:03:22 -0400

    Attempt to infer the slot a skill belongs to in skill_menu.py

--- processors/skill_menu.py
+++ processors/skill_menu.py
@@ -25,12 +25,12 @@ class SkillMenuProcessor(esper.Processor):
             skill_slot = event.get('skill_slot')
 
             equipped_items = self.world.component_for_entity(ent, EquipmentComponent).equipment
+            skill_directory = self.world.component_for_entity(ent, SkillDirectoryComponent).skill_directory
 
             if skill_letter:
                 slot = KEY_TO_SLOTS[skill_letter]
 
                 entire_skill_list = self.world.table_slot_skill[slot] # This is the full list of possible skills for this slot.
-                skill_directory = self.world.component_for_entity(ent, SkillDirectoryComponent).skill_directory
 
                 mastered_list = []
                 unmastered_list = []
@@ -63,9 +63,13 @@ class SkillMenuProcessor(esper.Processor):
                 self.world.get_processor(StateProcessor).queue.put({'popup': menu})
             
             elif skill_activate and skill_slot:
-                print(f"Attempting to activate {skill_activate}")
-                
-
-                # Activate the named skill
-                # Deactive the active skill for the slot, if there is one.
-                ### How do I look up which skills are active?
+                # Infer slot. Oof, this got ugly.
+                for slot, skills in self.world.table_slot_skill.items():
+                    if slot == skill_slot:
+                        for skill in skill_directory:
+                            if skill.name in skills:
+                                skill.is_active = False
+                                if skill_activate == skill.name:
+                                    skill.is_active = True
+                    elif skill_activate not in skills:
+                        print("ERROR Skill slots are not lining up with jsons and data tables.")
